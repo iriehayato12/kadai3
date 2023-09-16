0159a1cb05bd78145f18e2aeac210493a0c5fe52
iriehayato <143862129+iriehayato12@users.noreply.github.com>
2023-09-16 07:15:36 +0000

    課題3

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,17 @@
+class BooksController < ApplicationController
+  def new
+  @book = Book.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+  
+  def create
+  end
+  
+  def destroy
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def show
+  @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  def index
+  @users = User.find(params[:id])
+  end
+  
+  private
+  
+  def user_params
+  params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+end
